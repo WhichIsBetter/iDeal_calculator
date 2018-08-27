4b90f45a09acd129e490c5f043b5b68a25a92b1b
WhichIsBetter <mickael+code@mickaelng.com>
2018-08-27 19:19:51 +0800

    code updated

--- calculations.js
+++ calculations.js
@@ -1,3 +1,4 @@
+//dafault
 function defaultTemplate(flatK) {
   if (isNaN(flatK) == false) {
 
@@ -7,8 +8,8 @@ function defaultTemplate(flatK) {
   }
 }
 
+//inputs mm and returns Dioptre n ViceVersa
 function convertDmm(input) {
-  //inputs mm and returns Dioptre n ViceVersa
   if (isNaN(input) == false ) {
     return 337.5/input;
   }
@@ -17,10 +18,13 @@ function convertDmm(input) {
   }
 }
 
+//determines input is mm or dioptre and returns Dioptre
 function mmOrDioptre(input) {
-  //determines input is mm or dioptre and returns Dioptre
   if (isNaN(input) == false) {
-    if (input < 20) {
+    if (input == "") {
+      return null;
+    }
+    else if (input < 20) {
       // input is mm, convert to Diopter
       var value = convertDmm(input);
       return value;
@@ -39,8 +43,24 @@ function mmOrDioptre(input) {
   }
 }
 
+// decide FlatK -- K1/K2 must be in Dioptres
+function chooseFlatK(K1,K2) {
+  if (K1 == null && K2 == null) {
+    return null;
+  }
+  else if(K1 == null) {
+    return K2;
+  }
+  else if(K2 == null){
+    return K1;
+  }
+  else {
+    Math.min(K1, K2);
+  }
+}
+
+// vertex(mm) conversion of power(Diopters)
 function convertVertex(vertex,power) {
-  // vertex(mm) conversion of power(Diopters)
   if (isNAN(vertex) == false && isNaN(power) == false) {
     converted = power/(1-((vertex/1000)*power));
     return converted
@@ -50,8 +70,8 @@ function convertVertex(vertex,power) {
   }
 }
 
+//calculate iDealHydrogel BC -- flatK must be in mm
 function iDH_BC(flatK) {
-  //calculate iDealHydrogel BC
   if (isNaN(flatK) == false) {
     var bc = (flatK*0.6)+3.5;
     bc = Math.round(bc/0.2)*0.2; //round to nearest 0.2mm
@@ -68,6 +88,7 @@ var px = {
     "OD":{
       "K1":"",
       "K2":"",
+      "FlatK":"",
       "SPH":"",
       "CYL":"",
       "AXS":"",
@@ -76,6 +97,7 @@ var px = {
     "OS":{
       "K1":"",
       "K2":"",
+      "FlatK":"",
       "SPH":"",
       "CYL":"",
       "AXS":"",
@@ -99,3 +121,20 @@ var px = {
     }
   }
 };
+
+//calculate lens param for iDealHydrogel
+//px is object
+function iDH_Calc(px) {
+  //data clean up
+  px.input.OD.K1 = mmOrDioptre(px.input.OD.K1);
+  px.input.OD.K2 = mmOrDioptre(px.input.OD.K2);
+  px.input.OS.K1 = mmOrDioptre(px.input.OS.K1);
+  px.input.OS.K2 = mmOrDioptre(px.input.OS.K2);
+
+  px.input.OD.FlatK = chooseFlatK(px.input.OD.K1,px.input.OD.K2);
+  px.input.OS.FlatK = chooseFlatK(px.input.OS.K1,px.input.OS.K2);
+
+  //ODflatK = max()
+  //px.output.OD.BC =
+  return px;
+}
