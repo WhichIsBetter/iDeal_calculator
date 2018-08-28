fa54f717a25da475f2dbe12ef905a5533135c120
WhichIsBetter <mickael+code@mickaelng.com>
2018-08-28 19:53:49 +0800

    Update

--- calculations.js
+++ calculations.js
@@ -18,6 +18,18 @@ function convertDmm(input) {
   }
 }
 
+//determines input is deg or dioptre and returns Dioptre
+function degOrDioptre(input) {
+  input = Number(input);
+  if (Number.isInteger(input)) { //number is degree
+    return input/100;
+  }
+  else { //number is Diopter
+    return input;
+  }
+}
+
+
 //determines input is mm or dioptre and returns Dioptre
 function mmOrDioptre(input) {
   if (isNaN(input) == false) {
@@ -43,6 +55,23 @@ function mmOrDioptre(input) {
   }
 }
 
+function axsCleanUp(cyl,axs) {
+  if (Number(cyl) == 0 ) {
+    return "";
+  }
+  else if  (Number(cyl) < 0 ) {
+    if (Number(axs) == 0 ){
+      return 180;
+    }
+    else if (Number(axs) < 180) {
+      return axs
+    }
+    else {
+      return axs;
+    }
+  }
+}
+
 // decide FlatK -- K1/K2 must be in Dioptres
 function chooseFlatK(K1,K2) {
   if (K1 == null && K2 == null) {
@@ -54,14 +83,17 @@ function chooseFlatK(K1,K2) {
   else if(K2 == null){
     return K1;
   }
+  else if (K1 <= K2) {
+    return K1;
+  }
   else {
-    Math.min(K1, K2);
+    return K2;
   }
 }
 
 // vertex(mm) conversion of power(Diopters)
 function convertVertex(vertex,power) {
-  if (isNAN(vertex) == false && isNaN(power) == false) {
+  if (isNaN(vertex) == false && isNaN(power) == false) {
     converted = power/(1-((vertex/1000)*power));
     return converted
   }
@@ -75,10 +107,39 @@ function iDH_BC(flatK) {
   if (isNaN(flatK) == false) {
     var bc = (flatK*0.6)+3.5;
     bc = Math.round(bc/0.2)*0.2; //round to nearest 0.2mm
-    return bc;
+    return bc.toFixed(2);
   }
   else {
     return null;
+  }
+}
+
+//Generate Rx for print to text box
+function generateRx(SPH,CYL,AXS) {
+  if (SPH > 0) {
+    SPH = "+"+SPH.toFixed(2);
+  }
+  if(CYL == 0) {
+    return SPH.toFixed(2);
+  }
+  return SPH+"/"+CYL.toFixed(2)+"x"+AXS;
+}
+
+//Generate KReadings print to text box
+function generateKs(K1,K2) {
+  if (K1 == null && K2 == null) {
+    return "";
+  }
+  else if (K1 == null || K2 == null) {
+      if (K1 == null) {
+        return " "+K2.toFixed(2)+"mm, ";
+      }
+      else {
+        return " "+K1.toFixed(2)+"mm, ";
+      }
+  }
+  else {
+    return " "+K1.toFixed(2)+"mm/"+K2.toFixed(2)+"mm, ";
   }
 }
 
@@ -104,7 +165,7 @@ var px = {
       "HVID":""
     }
   },
-  "output":{
+  "iDH_output":{
     "OD":{
       "BC":"",
       "SPH":"",
@@ -131,10 +192,39 @@ function iDH_Calc(px) {
   px.input.OS.K1 = mmOrDioptre(px.input.OS.K1);
   px.input.OS.K2 = mmOrDioptre(px.input.OS.K2);
 
+  //calculate flatK
   px.input.OD.FlatK = chooseFlatK(px.input.OD.K1,px.input.OD.K2);
   px.input.OS.FlatK = chooseFlatK(px.input.OS.K1,px.input.OS.K2);
 
-  //ODflatK = max()
-  //px.output.OD.BC =
+  //calculate BC (iDH)
+  px.iDH_output.OD.BC = iDH_BC(convertDmm(px.input.OD.FlatK));
+  px.iDH_output.OS.BC = iDH_BC(convertDmm(px.input.OS.FlatK));
+
+  //vertex SPH
+  px.iDH_output.OD.SPH = convertVertex(px.input.vertex,px.input.OD.SPH);
+  px.iDH_output.OD.SPH = Math.round(px.iDH_output.OD.SPH/0.25)*0.25;
+  px.iDH_output.OD.SPH = px.iDH_output.OD.SPH.toFixed(2);
+  px.iDH_output.OS.SPH = convertVertex(px.input.vertex,px.input.OS.SPH);
+  px.iDH_output.OS.SPH = Math.round(px.iDH_output.OS.SPH/0.25)*0.25;
+  px.iDH_output.OS.SPH = px.iDH_output.OS.SPH.toFixed(2);
+
+  //vertex and calculate CYL
+  px.iDH_output.OD.CYL = convertVertex(px.input.vertex,(px.input.OD.SPH+px.input.OD.CYL))-px.iDH_output.OD.SPH;
+  px.iDH_output.OD.CYL = Math.round(px.iDH_output.OD.CYL/0.25)*0.25;
+  px.iDH_output.OD.CYL = px.iDH_output.OD.CYL.toFixed(2);
+  px.iDH_output.OS.CYL = convertVertex(px.input.vertex,(px.input.OS.SPH+px.input.OS.CYL))-px.iDH_output.OS.SPH;
+  px.iDH_output.OS.CYL = Math.round(px.iDH_output.OS.CYL/0.25)*0.25;
+  px.iDH_output.OS.CYL = px.iDH_output.OS.CYL.toFixed(2);
+
+  // Adds axs
+  px.iDH_output.OD.AXS = axsCleanUp(px.iDH_output.OD.CYL,px.input.OD.AXS);
+  px.iDH_output.OS.AXS = axsCleanUp(px.iDH_output.OS.CYL,px.input.OS.AXS);
+
+  //calculate HVID
+  px.iDH_output.OD.DIA = Math.round((px.input.OD.HVID+2.5)/0.2)*0.2;
+  px.iDH_output.OD.DIA = px.iDH_output.OD.DIA.toFixed(1);
+  px.iDH_output.OS.DIA = Math.round((px.input.OS.HVID+2.5)/0.2)*0.2;
+  px.iDH_output.OS.DIA = px.iDH_output.OS.DIA.toFixed(1);
+
   return px;
 }
